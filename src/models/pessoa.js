0409711b83ed86b9edb4247051502605e0d6bcdd
Patrick Simões <patricksimoes25@gmail.com>
2025-03-04 17:06:31 -0300

    Revert "chore: update git ignore"
    This reverts commit 4f393f8da0de02af747a632091f914e75a8e6562.

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -4,15 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      Pessoa.hasMany(models.Curso, {
-        foreignKey: 'docente_id'
-      });
-      Pessoa.hasMany(models.Matricula, {
-        foreignKey: 'estudante_id',
-        // scope: { status: 'matriculado' },
-        as: 'aulasMatriculadas'
-      });
+      // define association here
     }
   }
   Pessoa.init({
